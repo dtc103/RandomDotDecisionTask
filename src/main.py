4f42f98113eeb85c01098ad66fe65cecaf47330f
dtc103 <jan_kerner@gmx.de>
2024-07-17 22:07:11 +0200

    read out monitor size directly

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from RandomDotDecisionTask import RandomDotDecisionTask
 import tools
 
-import tkinter as tk
 
 #WIDTH, HEIGHT = 1980, 1080
-WIDTH, HEIGHT = 1368, 912
+#WIDTH, HEIGHT = 1368, 912
 MOTION_STRENGTHS = [0.0, 0.032, 0.064, 0.128, 0.256, 0.512]
 TRIALS = 50
 
 def main():
+    width, height = tools.get_monitor_dimensions()
     subject_name = input("Input the subject name: ")
 
-    task = RandomDotDecisionTask(WIDTH, HEIGHT, MOTION_STRENGTHS)
+    task = RandomDotDecisionTask(width, height, MOTION_STRENGTHS)
     task.run(TRIALS)
     task.quit()
 
@@ -19,12 +19,6 @@ def main():
         data_header = ["Decision Time [s]", "Success", "Coherence", "Actual Direction"]
         tools.save_data(task.answers, header=data_header, subject_name=subject_name)
     
-    # root = tk.Tk()
-    # root.update_idletasks()
-    # print(root.winfo_screenwidth())
-    # print(root.winfo_screenheight())
-    
-
 
 if "__main__" == __name__:
     main()
--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tkinter as tk
 
 def pos_to_angle(x, y):
     angle = np.arctan2(y, x) - np.pi 
@@ -13,3 +14,8 @@ def save_data(data, subject_name, header):
     data = pd.DataFrame(data)
 
     data.to_csv(f"C:/Users/jan_k/OneDrive - bwedu/Banburismus_presentation/RandomDotDecisionTask/data/{subject_name}.csv", index=False, header=header)
+    
+def get_monitor_dimensions():
+    root = tk.Tk()
+    root.update_idletasks()
+    return root.winfo_screenwidth(), root.winfo_screenheight()
